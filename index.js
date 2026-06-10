ddddf7cf83734146ef246cd7c90b4da1a2244663
monarchpressure-sketch <monarch.pressure@gmail.com>
2026-06-10 17:33:11 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,28 @@
 const express = require('express');
 const cors = require('cors');
-const fetch = require('node-fetch');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-const DUFFEL_TOKEN = 'duffel_test_JkQ2KRAb78johbeuE-xB6feWFTCDcC1U8A5r1jrMCEf';
-
 app.post('/api/search', async (req, res) => {
-  try     const response = await fetch('https://api.duffel.com/air/offer_requests', {
+  try {
+    const response = await fetch('https://api.duffel.com/air/offer_requests', {
       method: 'POST',
       headers: {
-        'Authorization': ?Bearer${DUFFEL_TOKEN}?,
+        'Authorization': ?Bearer ${process.env.DUFFEL_TOKEN}?,
         'Content-Type': 'application/json',
         'Duffel-Version': 'v2'
-      },
+   },
       body: JSON.stringify(req.body)
     });
-  const data = await response.json();
+    const data = await response.json();
     res.json(data);
   } catch (err) {
+    res.status(500).j{ error: err.message });
+  }
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log('VOY proxy running');
+});
